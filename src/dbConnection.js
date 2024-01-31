a40ec86dc4450f91cdab13ea6246c220c957abf5
Kaan <kaanasutay10@gmail.com>
2024-01-31 17:34:56 +0300

    call dbConnection

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -5,6 +5,6 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/')
+mongoose.connect(process.env.MONGODB || 'mongodb://localhost:27017/')
     .then(() => console.log(' * DB Connected * '))
     .catch((err) => console.log(' * DB Not Connected * ', err))
