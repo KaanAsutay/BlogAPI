a47c0c446986d9e0fd39b3a6ed6e15ea52cd9236
Kaan <kaanasutay10@gmail.com>
2024-02-03 01:54:34 +0300

    start controller

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,3 +2,35 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+// Catch async-errors and send to errorHandler
+require('express-async-errors')
+
+// Call Models:
+const {BlogPost} = require('../models/blogModel')
+
+// -------------------------------------------------------
+// BlogPost
+// -------------------------------------------------------
+module.exports.BlogPost = {
+    
+    list: async (req, res) => {
+        
+    },
+
+    create: async (req, res) => {
+        
+    },
+
+    read: async (req, res) => {
+        
+    },
+
+    update: async (req, res) => {
+        
+    },
+
+    delete: async (req, res) => {
+        
+    }
+}
