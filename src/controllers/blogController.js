978083188ae96f0652755cd01a9f1b5083409b62
Kaan <kaanasutay10@gmail.com>
2024-02-03 16:52:09 +0300

    list and create

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -15,11 +15,22 @@ const {BlogPost} = require('../models/blogModel')
 module.exports.BlogPost = {
     
     list: async (req, res) => {
+        const data = await BlogPost.find()
         
+        res.status(200).send({
+            error: false,
+            result: data
+        })
     },
 
     create: async (req, res) => {
-        
+        const data = await BlogPost.insertOne(req.body)
+
+        res.status(201).send({
+            error: false,
+            body: req.body,
+            result: data
+        })
     },
 
     read: async (req, res) => {
