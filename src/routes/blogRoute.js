fbc230a6383ff4654e3edc91d2396f1b5287935b
Kaan <kaanasutay10@gmail.com>
2024-02-03 16:56:03 +0300

    blogRoute

--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -2,3 +2,17 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+const { BlogPost } = require('../controllers/blogController')
+
+// -------------------------------------------------------
+// BlogPost
+// -------------------------------------------------------
+
+Router.route('/post')
+    .get(BlogPost.list)
+    .post(BlogPost.create)
+Router.route('/post/:postId')
+    .get(BlogPost.read)
+    .put(BlogPost.update)
+    .delete(BlogPost.delete)
