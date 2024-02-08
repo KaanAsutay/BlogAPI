b8c9e16c7ae7110152eac3a6386e98248654486e
Kaan <kaanasutay10@gmail.com>
2024-02-08 23:23:54 +0300

    userModel.js finish

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,3 +2,28 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: [true, 'Email field must be required.']
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    firstName: String,
+
+    lastName: String,
+
+
+
+}, {collection: 'users', timestamps: 'true'})
