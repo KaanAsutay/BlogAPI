61fd4318a4b69fbae08f4f1a01ff4b5cffd7a791
Kaan <kaanasutay10@gmail.com>
2024-02-09 01:04:12 +0300

    connect model and passwordEncrypt

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,6 +4,7 @@
 ------------------------------------------------------- */
 
 const mongoose = require('mongoose')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
 
 const UserSchema = new mongoose.Schema({
 
@@ -22,7 +23,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: true,
-        set: (password) => 'kaan'
+        set: (password) => passwordEncrypt
     },
 
     firstName: String,
