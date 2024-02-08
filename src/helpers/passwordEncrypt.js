438fd8efa0d79d64d0f35a0e87da1e059dd2ae70
Kaan <kaanasutay10@gmail.com>
2024-02-09 00:59:00 +0300

    consts

--- src/helpers/passwordEncrypt.js
+++ src/helpers/passwordEncrypt.js
@@ -4,3 +4,8 @@
 ------------------------------------------------------- */
 // Password Encrypt:
 const crypto = require('node:crypto')
+
+const keyCode = process.env.SECRET_KEY || 'write_random_chars_to_here'
+const loopCount = 10_000
+const charsCount = 32 // write 32 for 64
+const encType = 'sha512'
