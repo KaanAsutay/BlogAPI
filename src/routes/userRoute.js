88cb830487b56f899715525417553e2d368559ed
Kaan <kaanasutay10@gmail.com>
2024-02-08 23:30:18 +0300

    userRoute

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,3 +6,15 @@
 const router = require('express').Router()
 
 const { User } = require('../controllers/userController')
+
+// -------------------------------------------------------
+// User
+// -------------------------------------------------------
+
+router.route('/user')
+    .get(User.list)
+    .post(User.create)
+router.route('/user/:userId')
+    .get(User.read)
+    .put(User.update)
+    .delete(User.delete)
