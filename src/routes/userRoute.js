bccc2ccb5177100ce60696f5b2c13b9e2305824f
Kaan <kaanasutay10@gmail.com>
2024-02-08 23:29:12 +0300

    userRoute.js start

---
+++ src/routes/userRoute.js
@@ -0,0 +1,8 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const router = require('express').Router()
+
+const { User } = require('../controllers/userController')
