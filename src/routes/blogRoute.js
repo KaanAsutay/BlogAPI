9e786785b8e75c03edaaa975fabdc019096d6d70
Kaan <kaanasutay10@gmail.com>
2024-02-08 22:17:32 +0300

    blogCategory Route finish

--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -6,7 +6,19 @@
 const router = require('express').Router()
 
 // Call Controllers:
-const { BlogPost } = require('../controllers/blogController')
+const {BlogCategory, BlogPost } = require('../controllers/blogController')
+
+// -------------------------------------------------------
+// BlogCategory
+// -------------------------------------------------------
+
+router.route('/post')
+    .get(BlogCategory.list)
+    .post(BlogCategory.create)
+router.route('/post/:postId')
+    .get(BlogCategory.read)
+    .put(BlogCategory.update)
+    .delete(BlogCategory.delete)
 
 // -------------------------------------------------------
 // BlogPost
