ec2f6d5e06c0f0607dc7ee604dbace47ac5deaae
Kaan <kaanasutay10@gmail.com>
2024-02-09 01:02:47 +0300

    passwordEncrypt

--- src/helpers/passwordEncrypt.js
+++ src/helpers/passwordEncrypt.js
@@ -9,3 +9,8 @@ const keyCode = process.env.SECRET_KEY || 'write_random_chars_to_here'
 const loopCount = 10_000
 const charsCount = 32 // write 32 for 64
 const encType = 'sha512'
+
+module.exports = function (password) {
+    const encode = crypto.pbkdf2(password, keyCode, loopCount, charsCount, encType) // return BufferType
+    return encode.toString('hex')
+}
