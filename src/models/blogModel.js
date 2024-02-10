0038500e9d34ea05d45a7dd3c9df4e17151d8c38
Kaan <kaanasutay10@gmail.com>
2024-02-10 23:53:56 +0300

    sync blogCategory

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -56,6 +56,12 @@ const blogPostSchema = new mongoose.Schema({
 
     // _id
 
+    userId: {
+        type: mongoose.Schema.ObjectId, // Relational ObjectId
+        ref: 'BlogCategory', // ModelName
+        required: true
+    },
+
     blogCategoryId: {
         type: mongoose.Schema.ObjectId, // Relational ObjectId
         ref: 'BlogCategory', // ModelName
