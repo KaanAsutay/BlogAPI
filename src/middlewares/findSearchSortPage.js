34f34b880b61fd9aae042fa800ba851309df8775
Kaan <kaanasutay10@gmail.com>
2024-02-15 14:29:31 +0300

    start middlewares

---
+++ src/middlewares/findSearchSortPage.js
@@ -0,0 +1,8 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+module.exports = (req, res, next) => {
+    
+}
